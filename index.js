8f596356cfe4b4444513a712e435724c8aa41ff6
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-04-25 14:28:01 +0530

    Dice game by using Random number ganerator

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-
-/*
-let side1 =window.prompt("Enter side1= ");
-side1 = Number(side1);
-
-let side2 =window.prompt("Enter side2= ");
-side2 = Number(side2);
-
-let side3 = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
-
-console.log(side3+ " is side 3");
-*/
-
-//Another way to do, on web page:
-
 document.querySelector("button").onclick = function(){
-    let side1 = document.querySelector("#side1").value;
-    side1 = Number(side1);
-    let side2 = document.querySelector("#side2").value;
-    side2 = Number(side2);
+    let p1 = Math.floor(Math.random()*6 +1);
+    let p2 = Math.floor(Math.random()*6 +1);
 
-    let side3 = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
-
-    document.getElementById("side3Label").innerHTML = "Side 3: "+side3;
+    document.getElementById("p1").innerHTML = "Player1: "+p1;
+    document.getElementById("p2").innerHTML = "Player2: "+p2;
+    
 }
