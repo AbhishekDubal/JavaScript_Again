f212a792c320348e8c4de38878d72f95d851ae7b
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-04-26 01:40:12 +0530

    do while loop

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //while loop:-
 
-let sername = "";
-while(sername == "" || sername == null){
-    sername = window.prompt("Enter your name: ");
-}
-console.log("HEllo "+sername);
+// let sername = "";
+// while(sername == "" || sername == null){
+//     sername = window.prompt("Enter your name: ");
+// }
+// console.log("HEllo "+sername);
+
+let name ;
+
+do {
+    name=window.prompt("Enter Your NAME: ")
+} while (name == "");
+console.log("HEY", name);
